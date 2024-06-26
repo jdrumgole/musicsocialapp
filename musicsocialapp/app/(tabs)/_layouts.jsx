b31110734b2452b15e10dfc61034e913d4dd2167
jdrumgole <85814044+jdrumgole@users.noreply.github.com>
2024-06-26 18:21:50 -0400

    Tabs 	new file:   musicsocialapp/app/(tabs)/_layouts.jsx 	new file:   musicsocialapp/app/(tabs)/explore.jsx 	new file:   musicsocialapp/app/(tabs)/home.jsx 	new file:   musicsocialapp/app/(tabs)/profile.jsx 	modified:   musicsocialapp/app/_layout.tsx 	modified:   musicsocialapp/app/index.tsx 	modified:   musicsocialapp/constants/Colors.ts

---
+++ musicsocialapp/app/(tabs)/_layouts.jsx
@@ -0,0 +1,23 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import { Tabs } from 'expo-router'
+import { Ionicons } from '@expo/vector-icons';
+import {Colors} from './../../constants/Colors'
+
+
+export default function TabLayouts() {
+    return (
+        <Tabs screenOptions={{
+            headerShown: false,
+            tabBarActiveTintColor:Colors.PRIMARY
+        }}>
+            <Tabs.Screen name='home'
+                options={{
+                    tabBarLabel: 'Home',
+                    tabBarIcon:({color})=><Ionicons name="home" size={24} color={color} />
+            }}/>
+            <Tabs.Screen name='explore' />
+            <Tabs.Screen name='profile' />
+        </Tabs>
+    )
+}
